d224fb801e89389f4c1e6662a2e1d2b570d33e2a
papyntown <realistedem@gmail.com>
2023-02-10 10:02:23 +0100

    musculation-app

---
+++ components/Meta.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+const Meta = ({ title, description }) => {
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+        </Head>
+    );
+};
+Meta.defaultProps = {
+    title: "Strength Training Exercise Library with Animated Gifs & Descriptions",
+    description:
+        "Discover the best strength training exercises with our comprehensive list. Each exercise comes with a clear description and helpful animated gifs to guide you through proper form and technique. Improve your workout and build muscle with our user-friendly exercise library.",
+};
+
+export default Meta;
