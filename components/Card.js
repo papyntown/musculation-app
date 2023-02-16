fa06189a45ad6ade622e7fadca96896068cd4433
papyntown <realistedem@gmail.com>
2023-02-16 18:02:56 +0100

    0.2

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Card = ({ exercice }) => {
     return (
-        <div className="card-exercice">
-            <Image
-                src={`${exercice.gifUrl}`}
-                alt={exercice.name}
-                width={400}
-                height={400}
-                className=""
-            />
-            {exercice.name}
-        </div>
+        <Link href={`/exercice/[id]`} as={`exercice/${exercice.id}`}>
+            <div className="card-exercice">
+                <Image
+                    src={`${exercice.gifUrl}`}
+                    alt={exercice.name}
+                    width={400}
+                    height={400}
+                    className=""
+                />
+                <div className="card-target">
+                    <button className=" btn first-btn">
+                        {exercice.bodyPart}
+                    </button>
+                    <button className=" btn  second-btn">
+                        {exercice.target}
+                    </button>
+                </div>
+                <h3 className="exercice-name font-GothamBold">
+                    {exercice.name.charAt(0).toUpperCase() +
+                        exercice.name.slice(1)}
+                </h3>
+            </div>
+        </Link>
     );
 };
 
--- components/Exercices.js
+++ components/Exercices.js
@@ -7,6 +7,7 @@ const Exercices = ({ exerciseData, exercices, bodyPartList }) => {
         (state) => state.bodyPartFilter.bodyPartFilter
     );
     const [scrollCard, setscrollCard] = useState(12);
+    console.log(exercices);
 
     useEffect(() => {
         const watcher = document.getElementById("watcher");
@@ -19,20 +20,22 @@ const Exercices = ({ exerciseData, exercices, bodyPartList }) => {
     };
     return (
         <div className="containers ">
-            <div className="card-containers  mx-auto  w-11/12 grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 lg:max-w-8xl   gap-y-10 gap-x-8 px-4 py-8 ">
+            <div className="card-containers   mx-auto  w-11/12 grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 lg:max-w-8xl   gap-y-10 gap-x-8 px-4 py-8 ">
                 {/* Je veux un map d'exercices qui m'affiche les 10 premiers */}
 
                 {bodyPartFilter === "All"
                     ? exercices.length > 0
                         ? exercices
                         : exerciseData
+
                               .slice(0, scrollCard)
                               .map((exercice, index) => (
                                   <Card key={exercice.id} exercice={exercice} />
                               ))
                     : exercices.length > 0
-                    ? exercices
+                    ? { exercices }
                     : exerciseData
+
                           .filter((exercice) =>
                               exercice.bodyPart.includes(bodyPartFilter)
                           )
