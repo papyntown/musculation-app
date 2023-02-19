bbb117b05344e3078d045020c3b3259eca8294c4
papyntown <realistedem@gmail.com>
2023-02-19 16:35:40 +0100

    0.8

---
+++ components/ExerciseVideos.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ExerciseVideos = ({ exerciceVideos, name }) => {
+    console.log(exerciceVideos);
+    return (
+        <div className="videos-container">
+            <h3>
+                Watch <span>{name}</span> exercices videos
+            </h3>
+            <div className="video">
+                {exerciceVideos?.map((video, index) => (
+                    <a
+                        key={index}
+                        href={`https://www.youtube.com/watch?v=${video.video.videoId}`}
+                        className="exercice-video"></a>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ExerciseVideos;
