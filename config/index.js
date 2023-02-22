600bbd2ba9bc79207c333f812bb5fb7709c56983
papyntown <realistedem@gmail.com>
2023-02-22 12:36:13 +0100

    img

--- config/index.js
+++ config/index.js
@@ -1,1 +1,2 @@
-export const path = "http://localhost:3000";
+// export const path = "http://localhost:3000";
+export const path = "https://fitforces.vercel.app";
--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-        ],
+        domains: ["localhost"],
+        unoptimized: true,
     },
 };
 
